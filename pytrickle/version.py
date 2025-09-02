1e7f91032a25d359bad764b74b990a645ebaedab
John | Elite Encoder <john@eliteencoder.net>
2025-09-02 11:08:06 -0400

    use semantic versioning for pkg and /version, add /app-version (#26)
    * use semver, return pkg ver for `/version`, add `/app-version` for pipeline

---
+++ pytrickle/version.py
@@ -0,0 +1,9 @@
+"""
+Version management for pytrickle package.
+
+This module provides version information used by both __init__.py and server.py.
+"""
+
+# Import from generated _version.py file (created by setuptools-scm)
+from ._version import version as __version__
+VERSION = __version__
